9e7cdf66dc3e0951819fa722592085b01a38e445
Sahin Hossen <sahinhossen21@gmail.com>
2018-11-12 19:25:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Popover JS
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+// Popover JS
+
+// Popover Dismiss JS
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+})
+// Popover Dismiss JS
+
+// Tooltip JS
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+// Tooltip JS
